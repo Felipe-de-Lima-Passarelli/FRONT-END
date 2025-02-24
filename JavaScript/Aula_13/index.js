78872696b45998353488e57035bdd9b732681268
Felipe <malamdorox@gmail.com>
2025-02-24 18:30:58 -0300

    Att

--- JavaScript/Aula_13/index.js
+++ JavaScript/Aula_13/index.js
@@ -4,7 +4,11 @@ console.log(alunos[0]);
 console.log(alunos[1]);
 console.log(alunos[2]);
 
-alunos.push("Otávio");
-alunos.unshift("Luiza");
-alunos.pop();
+alunos.push("Otávio"); // Adicionar ao final
+alunos.pop(); // Remover do final
+alunos.unshift("Luiza"); // Adicionar no começo
+alunos.shift(); // Remover do começo
+delete alunos[1]; // Remove o conteúdo mas mantém o index
 console.log(alunos);
+console.log(alunos.slice(0, 2));
+console.log(alunos instanceof Array); // Verifica se alunos é um Array
