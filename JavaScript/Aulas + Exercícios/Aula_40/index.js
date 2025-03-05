247dbc50d7e77474c644d0c3c4c026f85ea32e67
Felipe <malamdorox@gmail.com>
2025-03-05 16:57:01 -0300

    Att 05/03/2025

---
+++ JavaScript/Aulas + Exercícios/Aula_40/index.js
@@ -0,0 +1,6 @@
+const nome = "Felipe de Lima Passarelli";
+const arrayNome = nome.split(" "); //Transforma uma String em um Array
+const nomeArray = arrayNome.join(" "); //Transforma um Array em uma String
+
+console.log(arrayNome);
+console.log(nomeArray);
