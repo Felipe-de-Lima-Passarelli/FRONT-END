aa440482def0dcb2a121da633ea159c668ad81be
Felipe <malamdorox@gmail.com>
2025-06-06 09:22:35 -0300

    Att React

--- JavaScript/Estudos React IA/AULAS/Aula_4/src/App.jsx
+++ JavaScript/Estudos React IA/AULAS/Aula_4/src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import FormState from "./components/FormState";
+import FormularioContato from "./components/FormularioContato";
 
 function App() {
-  return <h1>Início</h1>;
+  return (
+    <div>
+      <h1>Formulário</h1>
+      <br />
+      {/* <FormState /> */}
+      <FormularioContato />
+    </div>
+  );
 }
 
 export default App;
