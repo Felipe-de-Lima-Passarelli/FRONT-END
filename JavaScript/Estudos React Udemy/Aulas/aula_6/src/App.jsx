19202a115928f2db2495d5ece200edced83b2a06
Felipe <malamdorox@gmail.com>
2025-08-25 10:44:40 -0300

    Att React

--- JavaScript/Estudos React Udemy/Aulas/aula_6/src/App.jsx
+++ JavaScript/Estudos React Udemy/Aulas/aula_6/src/App.jsx
@@ -1,9 +1,22 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
     <div>
       <h1>Context API</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route />
+          <Route />
+          <Route />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
