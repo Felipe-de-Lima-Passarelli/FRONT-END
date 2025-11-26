f0958e34fc64851e62959dcf0d3961f5bb2279ec
Felipe <malamdorox@gmail.com>
2025-11-26 20:56:57 -0300

    Att

--- Curso Web Frontend/Aulas/Aula 17. JavaScript Intermediário - DOM/index.js
+++ Curso Web Frontend/Aulas/Aula 17. JavaScript Intermediário - DOM/index.js
@@ -1,1 +1,24 @@
 //1. Selecionando elementos individuais
+
+/*
+    getElementById("ID") | querySelector("#ID")
+    getElementsByClassName("CLASS") | querySelector ("CLASS")
+    getElementsByTagName("TAG") | querySelector("TAG")
+*/
+
+const divPrincipal = document.getElementById("destaque");
+console.log(divPrincipal);
+
+const paragrafo = document.querySelector("p");
+console.log(paragrafo);
+
+function mudarTexto() {
+  const listaPrimeiro = document.getElementsByClassName("primeiro")[0];
+  listaPrimeiro.innerHTML = "Teste";
+  listaPrimeiro.classList.add("destaque");
+}
+
+const botao = document.getElementsByClassName("botao")[0];
+botao.addEventListener("click", mudarTexto);
+
+//2. Selecionando múltiplos elementos
