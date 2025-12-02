81bf7958d4a788a024457b3bd05a81cc8a8c8def
Felipe <malamdorox@gmail.com>
2025-12-02 15:19:08 -0300

    Att

--- Curso Web Frontend/Aulas/Aula 18. JavaScript Intermediário - Eventos/index.js
+++ Curso Web Frontend/Aulas/Aula 18. JavaScript Intermediário - Eventos/index.js
@@ -1,3 +1,64 @@
-//1. Eventos JS
+//1. Eventos JS - Interface
 
-a;
+/*
+
+Eventos de Interface do Usuário:
+. load -> A página web terminou de carregar
+. unload -> A página web está carregando
+. error -> O navegador encontra um erro
+. resize -> A janela do navegador foi redimensionada
+. scrool -> Usuário rolou a página para cima ou para baixo
+
+*/
+
+//2. Eventos JS - Teclado
+
+/*
+
+Eventos de Teclado:
+. keydown -> O usuário pressiona uma tecla (Mantem pressionada)
+. keyup -> Usuário solta uma tecla
+. keypress -> Caracteres estão sendo inseridos
+
+*/
+
+//3. Eventos JS - Mouse
+
+/*
+
+Eventos de Mouse:
+. click -> Pressiona e solta o botão sobre o mesmo elemento
+. dbclick -> Pressiona e solta o botão duas vezes
+. mousedown -> Pressiona o botãao do mouse sobre um elemento
+. mouseup -> Solta o botão do mouse sobre um elemento
+. mousemove -> Move o mouse
+. mouseover -> Move o mouse sobre um elemento
+. mouseout -> Move o mouse para fora do elemento
+
+*/
+
+//4. Eventos JS - Focus-Blur
+
+/*
+
+Eventos de Focus:
+. focus -> O elemento ganha o foco
+. blur -> O elemento perde o foco
+
+*/
+
+//5. Eventos JS - Formulário
+
+/*
+
+Eventos de Formulário:
+. input -> Valor  de qualquer elemento <input> mudou
+. change -> O valor em uma caixa de seleção ou botão mudou
+. submit -> Usuário envia um formulário
+. reset -> Usuário clica em um botão reset
+. cut -> Usuário "recorta" o conteúdo de um campo de formulário
+. copy -> Usuário "copia" o conteúdo de um campo de formulário
+. paste -> Usuário "cola" o conteúdo de um campo de formulário
+. select -> Usuário "seleciona" um texto em um campo de formulário
+
+*/
