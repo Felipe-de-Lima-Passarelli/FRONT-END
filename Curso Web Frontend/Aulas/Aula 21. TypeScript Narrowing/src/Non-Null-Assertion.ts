faf2225fd7211aa171157716b151d13ed2ff0217
Felipe <malamdorox@gmail.com>
2025-12-06 03:24:48 -0300

    Att TypeScript

---
+++ Curso Web Frontend/Aulas/Aula 21. TypeScript Narrowing/src/Non-Null-Assertion.ts
@@ -0,0 +1,6 @@
+//Utilização do "!" ao final evitar o retorno de ser um falsy
+const title = document.getElementById("title")!;
+console.log(title); //HTMLElement
+
+const subtitle = document.getElementById("subtitle");
+console.log(subtitle); //HTMLElement ou Null
---
+++ Curso Web Frontend/Aulas/Aula 21. TypeScript Narrowing/src/Optional Chaining.ts
@@ -0,0 +1,4 @@
+const title = document.getElementById("title");
+
+// "?" permite evitar o erro no console caso não exista title
+console.log(title?.innerHTML);
